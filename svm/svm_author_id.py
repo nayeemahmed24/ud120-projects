01d3bc08a322a6fd6cb18090629f646c34ce9a2b
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2019-04-27 11:17:51 +0600

    naive bayes and svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,23 +7,37 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+
+from sklearn.svm import SVC
+import numpy as np
+
+clf = SVC(kernel="rbf", C=10000.0)
+features_train = features_train[int(0):int(len(features_train) / 100)]
+labels_train = labels_train[int(0):int(len(labels_train) / 100)]
+t0 = time()
+clf.fit(features_train, labels_train)
+print(round(time() - t0, 3))
+# prettyPicture(clf, features_test, labels_test)
+# output_image("test.png", "png", open("test.png", "rb").read())
+pred = clf.predict(features_test)
+a = clf.score(features_test, labels_test)
+aa = np.count_nonzero(pred)
+
+print(aa)
 
 #########################################################
 
