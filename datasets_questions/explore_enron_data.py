64a880792f1adaf04b1fae4f275ef447ab75aa34
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2019-05-21 01:00:14 +0600

    Enron Dataset Loaded

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,14 @@
 """
 
 import pickle
+#authors_file_handler = open(authors_file, "rb")
+#    authors = pickle.load(authors_file_handler, fix_imports=True)
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+#enron = open("../final_project/final_project_dataset.pkl", "rb")
+#enron_data = pickle.load(enron, fix_imports=True)
+en = r'G:\udacity course git\ud120-projects\final_project\final_project_dataset_unix.pkl'
+enron = open(en, "rb")
+enron_data = pickle.load(enron,fix_imports=True)
+print(len(enron_data))
 
 
